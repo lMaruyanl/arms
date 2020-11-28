5a77cf5035487a0990076f5a1f617921429d282e
Unohana13 <74942125+Unohana13@users.noreply.github.com>
2020-11-28 23:38:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -795,7 +795,7 @@ html{
     </div>
     <div class="col-sm-4">
       <span class="glyphicon glyphicon-tint logo-small"></span>
-      <h4>FREE WATER</h4>
+      <h4>WATER</h4>
       <p>In one drop of water are found all the secrets of all the oceans; in one aspect of You are found all the aspects of existence. That's why it's free </p>
     </div>
   </div>
@@ -855,6 +855,96 @@ html{
       </div>
     </div>
   </div>
+
+<!-- location3 -->
+  <div id="mylocation3" class="carousel slide" data-ride="carousel" style="top:-29px;">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#mylocation3" data-slide-to="0" class="active"></li>
+      <li data-target="#mylocation3" data-slide-to="1"></li>
+      <li data-target="#mylocation3" data-slide-to="2"></li>
+      <li data-target="#mylocation3" data-slide-to="3"></li>
+      <li data-target="#mylocation3" data-slide-to="4"></li>
+    </ol>
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner">
+
+      <div class="item active">
+        <img src="Carouselpic\location1.jpg" alt="first pic" style="width:auto; height:500px; margin:auto;">
+      </div>
+      <div class="item">
+        <img src="Carouselpic\location1\building1room1.jpg" alt="Second pic" style="width:auto; height:500px; margin:auto;">
+      </div>
+      <div class="item">
+        <img src="Carouselpic\location1\building1room2.jpg" alt="third pic"  style="width:auto; height:500px; margin:auto;">
+      </div>
+      <div class="item">
+        <img src="Carouselpic\location1\building1room3.jpg" alt="Fourth pic" style="width:auto; height:500px; margin:auto;">
+      </div>
+      <div class="item">
+        <img src="Carouselpic\location1\building1room4.jpeg" alt="Fifth pic" style="width:auto; height:500px; margin:auto;">
+      </div>
+    </div>
+
+    <!-- Left and right controls -->
+    <a class="left carousel-control" href="#mylocation1" data-slide="prev">
+      <span class="glyphicon glyphicon-chevron-left"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#mylocation1" data-slide="next">
+      <span class="glyphicon glyphicon-chevron-right"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+  <!-- location3 details -->
+<div class="building_text">
+  <div>
+      <h1> Location 3  Building Details</h1>
+  </div>
+  <div>
+     <p>Address: Pasig City</p>
+     <p>Income Requirment: Must have 2x the rent in the total household income</p>
+     <p>Pets: No pets are allowed</p>
+     <p>Fee: $150 per aparment</p>
+     <p>Rent: $45 for 1 per/month, $40 for 2pets/months</p>
+  </div>
+</div>
+<br></br>
+
+<!-- Container (Services Section) -->
+<div id="services" class="container-fluid text-center">
+      <br></br><br></br>
+  <h2>Location 3 AMMENITIES</h2>
+  <h4>What we offer</h4>
+  <div class="row slideanim">
+    <div class="col-sm-4" style="display:flex;">
+      <span class="glyphicon glyphicon-globe logo-small" style="display:flex;">
+      </span>
+      <div style="margin-left:10px;">
+      <h4 style="font-size:30px; margin-bottom:10px; font-farmily:robo;">FREE WIFI</h4>
+      <p class="amme">The building is a facility with a free wifi Access with an internet speed that matched up the standard data rate speed for researching</p>
+      </div>
+    </div>
+    <div class="col-sm-4" style="display:flex;">
+      <span class="glyphicon glyphicon-facetime-video logo-small" style="display:flex;">
+      </span>
+      <div style="margin-left:10px;">
+      <h4 style="font-size:30px; margin-bottom:10px; font-farmily:robo;">Cover with CCTV</h4>
+      <p class="amme">The building is a facility Covered with Cameras in every corner and hallway.</p>
+      </div>
+    </div>
+    <div class="col-sm-4" style="display:flex;">
+      <span class="glyphicon glyphicon-road logo-small" style="display:flex;">
+      </span>
+      <div style="margin-left:10px;">
+      <h4 style="font-size:30px; margin-bottom:10px; font-farmily:robo;">Free Parking</h4>
+      <p class="amme">The building has a large space for tenants with vehicle.</p>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+
 <!-- Terms and conditions -->
 <div id="terms" class="modal fade">
       <div class="modal-dialog">
