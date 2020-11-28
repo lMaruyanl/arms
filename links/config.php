9d9092c2ae3de3c334322537be449a0843530955
Unohana13 <74942125+Unohana13@users.noreply.github.com>
2020-11-28 11:31:57 +0800

    Update config.php

--- links/config.php
+++ links/config.php
@@ -1,9 +1,9 @@
 <?php
 error_reporting(0);
-define('DB_NAME', 'ARMS');
-define('DB_USER', 'root');
-define('DB_PASSWORD', '');
-define('DB_HOST', 'localhost');
+define('DB_NAME', 'heroku_04694c75558c49f');
+define('DB_USER', 'b05b9648d0c6c0');
+define('DB_PASSWORD', 'def94a3c');
+define('DB_HOST', 'us-cdbr-east-02.cleardb.com');
 
 // Create connection
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
