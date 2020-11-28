e7343c00abeda42fdb8c2c3ef0e8e6badcc37d10
Unohana13 <74942125+Unohana13@users.noreply.github.com>
2020-11-28 14:35:11 +0800

    Update operator.php

--- links/operator.php
+++ links/operator.php
@@ -917,7 +917,7 @@ $("#show-sidebar").click(function() {
       </div>
       <div class="sidebar-header">
         <div class="user-pic">
-          <img class="img-responsive img-rounded" src="../icon/admin.png"
+          <img class="img-responsive img-rounded" src="../icon/pic.png"
             alt="User picture">
         </div>
         <div class="user-info">
